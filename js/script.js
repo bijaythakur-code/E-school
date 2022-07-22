5fb517af96ccd7973b19e0224ca0b125f6456763
DESKTOP-HIGNV92\Bijay <bijaythakur330@gmail.com>
2022-07-22 14:32:54 +0545

    gitignore file added and minor changes

--- js/script.js
+++ js/script.js
@@ -14,34 +14,34 @@ btnNavEl.addEventListener('click', function () {
   headerEl.classList.toggle('nav-open')
 })
 
-///////////////////////////////////////////////////////////
-// Smooth scrolling animation
+// ///////////////////////////////////////////////////////////
+// // Smooth scrolling animation
 
-const allLinks = document.querySelectorAll('a:link')
+// const allLinks = document.querySelectorAll('a:link')
 
-allLinks.forEach(function (link) {
-  link.addEventListener('click', function (e) {
-    e.preventDefault()
-    const href = link.getAttribute('href')
+// allLinks.forEach(function (link) {
+//   link.addEventListener('click', function (e) {
+//     e.preventDefault()
+//     const href = link.getAttribute('href')
 
-    // Scroll back to top
-    if (href === '#')
-      window.scrollTo({
-        top: 0,
-        behavior: 'smooth',
-      })
+//     // Scroll back to top
+//     if (href === '#')
+//       window.scrollTo({
+//         top: 0,
+//         behavior: 'smooth',
+//       })
 
-    // Scroll to other links
-    if (href !== '#' && href.startsWith('#')) {
-      const sectionEl = document.querySelector(href)
-      sectionEl.scrollIntoView({ behavior: 'smooth' })
-    }
+//     // Scroll to other links
+//     if (href !== '#' && href.startsWith('#')) {
+//       const sectionEl = document.querySelector(href)
+//       sectionEl.scrollIntoView({ behavior: 'smooth' })
+//     }
 
-    // Close mobile naviagtion
-    if (link.classList.contains('nav-link'))
-      headerEl.classList.toggle('nav-open')
-  })
-})
+//     // Close mobile naviagtion
+//     if (link.classList.contains('nav-link'))
+//       headerEl.classList.toggle('nav-open')
+//   })
+// })
 
 ///////////////////////////////////////////////////////////
 // Sticky navigation
